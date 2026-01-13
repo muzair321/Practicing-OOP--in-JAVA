ef6b5685744a32b121ae42d3acafa63599d45c4a
Muhammad Uzair <muzairasim321@gmail.com>
2026-01-13 17:36:21 +0500

    Implement Person, Student, and Teacher classes
    Added Person, Student, and Teacher classes with constructors.

--- task6.java
+++ task6.java
@@ -1,1 +1,24 @@
-
+public class task6{
+  public static void main(String[] args){
+  }
+}
+class Person{
+  private String  name;
+  private int age;
+}
+class Student extends Person{
+  String roll;
+  Student(String name, int age, String roll){
+    super.name = name;
+    super.age = age;
+    this.roll = roll;
+  }
+}
+class Teacher extends Person{
+  String subject;
+  Student(String name, int age, String subject){
+    super.name = name;
+    super.age = age;
+    this.subject = subject;
+  }
+}

b37c68aab48b1df38224f4b8f261352e01b9592a
Muhammad Uzair <muzairasim321@gmail.com>
2026-01-13 13:33:35 +0500

    Implement user login functionality

--- task4.java
+++ task4.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class main{
+  public static Scanner inp = new Scanner(System.in);
+  public static void main(String[] args){
+    User uzi = new User("uzi2011","123");
+    uzi.login("uzi","123");
+    uzi.login("uzi2011","1234");
+    uzi.login("uzi2011","123");
+  }
+}
+class User{
+  private String user;
+  private String password;
+  User(String user, String password){
+    this.user = user;
+    this.password = password;
+  }
+  public void login(String user, String password){
+    if(this.user.equals(user) && this.password.equals(password)){
+      System.out.println("Logged In Successfully");
+    }else{
+      System.out.println("Invalid Credentials");
+    }
+  }
+}

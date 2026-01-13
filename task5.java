a2f404f17c57f1f703fd172fb99c66c3da5940e0
Muhammad Uzair <muzairasim321@gmail.com>
2026-01-13 17:30:00 +0500

    Refactor shape area calculation methods

--- task5.java
+++ task5.java
@@ -1,25 +1,35 @@
 public class task5{
   public static void main(String[] args){
-    Shape rect = new Rectangle();
-    Shape circ = new Circle();
-    System.out.println("Area Of Rectangle Is: " + rect.calculateArea(20,40));
-    System.out.println("Area Of Circle Is: " + circ.calculateArea(10));
+    Shape rect = new Rectangle(20,40);
+    Shape circ = new Circle(10);
+    System.out.println("Area Of Rectangle Is: " + rect.calculateArea());
+    System.out.println("Area Of Circle Is: " + circ.calculateArea());
   }
 }
 class Shape{
-  public double calculateArea(){
+  double calculateArea(){
     return 0;
   }
 }
-class Rectangle extends Area{
+class Rectangle extends Shape{
+  double len;
+  double wid;
+  Rectangle(double len, double wid){
+    this.len = len;
+    this.wid = wid;
+  }
   @Override
-  public double calculateArea(double len, double wid){
+  double calculateArea(){
     return len*wid;
   }
 }
-class Circle extends Area{
+class Circle extends Shape{
+  double r;
+  Circle(double r){
+    this.r = r;
+  }
   @Override
-  public double calculateArea(double r){
+  double calculateArea(){
     return r*r*Math.PI;
   }
 }

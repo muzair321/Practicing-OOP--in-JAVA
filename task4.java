ba21ac258e9628e49c7dc7abdc72c136584615b1
Muhammad Uzair <muzairasim321@gmail.com>
2026-01-13 13:44:06 +0500

    Refactor User login method to use input prompts

--- task4.java
+++ task4.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
-public class main{
-  public static Scanner inp = new Scanner(System.in);
+public class task4{
+  
   public static void main(String[] args){
     User uzi = new User("uzi2011","123");
-    uzi.login("uzi","123");
-    uzi.login("uzi2011","1234");
-    uzi.login("uzi2011","123");
+    uzi.login();
   }
+}
+class GlobalScanner{
+  public static Scanner inp = new Scanner(System.in);
 }
 class User{
   private String user;
@@ -16,8 +17,12 @@ class User{
     this.user = user;
     this.password = password;
   }
-  public void login(String user, String password){
-    if(this.user.equals(user) && this.password.equals(password)){
+  public void login(){
+    System.out.print("Enter User Name: ");
+    String inpUser = GlobalScanner.inp.nextLine();
+    System.out.print("Enter Password: ");
+    String inpPass = GlobalScanner.inp.nextLine();
+    if(user.equals(inpUser) && password.equals(inpPass)){
       System.out.println("Logged In Successfully");
     }else{
       System.out.println("Invalid Credentials");

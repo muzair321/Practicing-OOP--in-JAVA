bad7ebc63a18e42f1246917a5d39dde1a75ceb67
Muhammad Uzair <muzairasim321@gmail.com>
2026-01-15 09:40:19 +0500

    Refactor Library and Student classes for book management

--- project.java
+++ project.java
@@ -1,43 +1,60 @@
+import java.util.ArrayList;
+
 public class project{
   public static void main(String[] args){
+    Student stu = new Student();
+    stu.add("Unexpected");
+    stu.add("Rich Dad Poor Dad");
+    stu.available();
+    stu.issue("Rich Dad Poor Dad");
+    stu.available();
+    stu.returnBook("Rich Dad Poor Dad");
+    stu.available();
   }
 }
 class Book{
-  private ArrayList<String> bookList = new ArrayList<>[];
-  private ArrayList<boolean> avlblty = new ArrayList<>[];
+  ArrayList<String> bookList = new ArrayList<>();
+  ArrayList<Boolean> avlblty = new ArrayList<>();
 }
 class Library extends Book{
   void available(){
-    System.out.print("  Available Books");
+    System.out.println("  Available Books:");
     for(int i = 0; i < bookList.size(); i++){
-      System.out.println("  " + (i+1) + ". " + bookList.get(i));
+      System.out.print("  " + (i+1) + ". " + bookList.get(i));
+      if(avlblty.get(i) == true){
+        System.out.println(" - Available");
+      }else System.out.println(" - Not Available");
     }
   }
   void add(String book){
     bookList.add(book);
     avlblty.add(true);
-    System.out.print("  " + book + " added to collection");
+    System.out.println("  " + book + " added to collection");
   }
 }
 class Student extends Library{
   void issue(String bookName){
     for(int i = 0; i < bookList.size(); i++){
-      if(avlblty.get(i) == true){
-        if(bookName.equals(bookList.get(i)){
-          avlblty.get(i) = false;
+      if(bookName.equals(bookList.get(i))){
+        if(avlblty.get(i) == true){
+          avlblty.set(i, false);
           System.out.println("  Book Successfully Issued");
-        }else System.out.println("  Book Not Found");
-      }else System.out.println("  Book Not Available");
+          }else System.out.println("  Book Not Available");
+          return;
+        }
     }
+    System.out.println("  Book Not Found");
   }
-  void return(String bookName){
+  void returnBook(String bookName){
     for(int i = 0; i < bookList.size(); i++){
-      if(avlblty.get(i) == false){
-        if(bookName.equals(bookList.get(i)){
-          avlblty.get(i) = true;
+      if(bookName.equals(bookList.get(i))){
+        if(avlblty.get(i) == false){
+          avlblty.set(i, true);
           System.out.println("  Book Successfully Returned");
-        }else System.out.println("  Book Not Found");
-      }else System.out.println("  Book Already Returned");
+          }else System.out.println("  Book Already Returned");
+          return;
+        }
     }
+    System.out.println("  Book Not Found");
   }
 }
